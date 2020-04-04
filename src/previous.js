da41ff0101daa675c1af07609f7c991bae3ad2ee
mariaSmirnova12 <maria.smirnova@leadexsystems.ru>
2020-04-04 11:41:11 +0700

    commit test file

---
+++ src/previous.js
@@ -0,0 +1,10 @@
+export default previos
+/**
+ * This method will perform addition operation.
+ *
+ * @param {Array} array - the array
+ * @return {*} - last item in the array
+ */
+function previos(array) {
+  return array[array.length - 2]
+}
